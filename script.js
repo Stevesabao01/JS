89f92083b62508e6be851f771a33ecdf1b934111
Nico_Oliveira39 <98227758+Stevesabao01@users.noreply.github.com>
2023-08-25 09:13:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var Joao = 0;
+var Pedro = 0;
+var Rank;
+
+if (Joao = -2) 
+    if (Joao > 1) {
+        console.log('Joao marcou ponto!');
+} else if (Pedro > 0) {
+    console.log('Pedro marcou ponto!');
+} else {
+    console.log('Joao Ganhou!');
+}
